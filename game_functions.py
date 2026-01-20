ed46d6184d13358bd7bc2b2317584cc55d63df00
ar1151 <ar1151@exeter.ac.uk>
2026-01-20 15:47:41 +0000

    optimal pick_values method

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
+    x = poss_values[len(poss_values)//2]
     return x
 
 # function to be used in game_2: Higher or Lower
@@ -30,3 +30,6 @@ def process_guess(letter, board, word_list):
         return True
     print("Sorry, \'"+letter+"\' is not in the word")
     return False
+
+
+
